e064f988265f4cc5f966b4511bdf0a6bc81f8e7d
Ivan Tkachuk <git@tkachuk.email>
2024-01-27 17:04:56 +0200

    Model serializers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -6,18 +6,21 @@ from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     discount = serializers.SerializerMethodField(read_only=True)
-    url = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(
+        view_name="product-detail", lookup_field="pk"
+    )
 
     class Meta:
         model = Product
-        fields = ["url", "title", "content", "price", "sale_price", "discount"]
+        fields = ["url", "edit_url", "title", "content", "price", "sale_price", "discount"]
 
-    def get_url(self, obj: Product):
+    def get_edit_url(self, obj: Product):
         request = self.context.get("request")
         if request is None:
             return None
-        
-        return reverse("product-detail", kwargs={"pk": obj.pk}, request=request)
+
+        return reverse("product-edit", kwargs={"pk": obj.pk}, request=request)
 
     def get_discount(self, obj: Product):
         if isinstance(obj, Product):
